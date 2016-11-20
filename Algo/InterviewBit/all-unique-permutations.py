0b83fc825ed4e0d9be396ddbcbd1023714fffd82
Ayush Goel <ayushgoel111@gmail.com>
2016-11-20 15:58:38 +0530

    Fix solution for all-unique-permutations
    The new solution makes list hashable for our particular
    kind. The checking is done by a method. All other
    operations remain the same.

--- Algo/InterviewBit/all-unique-permutations.py
+++ Algo/InterviewBit/all-unique-permutations.py
@@ -1,34 +1,44 @@
 # https://www.interviewbit.com/problems/all-unique-permutations/
 
 class Solution:
-    def all_permutations(self, s):
-        if len(s) == 1:
-            return set(frozenset(s))
-        # calculate all permutations for s[1:] and place s[0] at all possible locations
-        # add the generated strings to a set and return the set
-        perms = set()
-        s_leaving_first_char = s[1:]
-        s_leaving_first_char_perms = self.all_permutations(s_leaving_first_char)
-        # print("X", s_leaving_first_char, s_leaving_first_char_perms)
-        for permutation_frozenset in s_leaving_first_char_perms:
-            permutation = iter(permutation_frozenset).next()
-            # print("Y", permutation)
+    def remove_dupes(self, arr):
+        # Get list of list of integers
+        # return list of list of integers, with duplicates removed.
+        no_dupes = set()
+        ans = []
+        for i in arr:
+            strarr = [str(j) for j in i]
+            stri = ':'.join(strarr)
+            if stri in no_dupes:
+                continue
+            else:
+                no_dupes.add(stri)
+                ans += [i]
+        return ans
+
+    def get_all_permutations(self, arr):
+        # input - list of integers
+        # return list of list of integers of permutation, all unique
+        if len(arr) < 1:
+            return []
+        if len(arr) == 1:
+            return [arr]
+        arr_without_first_number = arr[1:]
+        arr_without_first_number_perms = self.get_all_permutations(arr_without_first_number)
+        # print("A", arr_without_first_number_perms)
+        arr_without_first_number_perms = self.remove_dupes(arr_without_first_number_perms)
+        # print("B", arr_without_first_number_perms)
+        perms = []
+        for permutation in arr_without_first_number_perms:
             for i in xrange(len(permutation) + 1):
-                res = permutation[:i] + s[0] + permutation[i:]
-                # print("AA", i, res)
-                perms.add(frozenset([res]))
-        # print("Z", perms)
+                res = permutation[:i] + [arr[0]] + permutation[i:]
+                perms += [res]
         return perms
 
     # @param A : list of integers
     # @return a list of list of integers
     def permute(self, A):
-        if len(A) < 1:
-            return []
-        a = ''.join([str(i) for i in A])
-        perms = self.all_permutations(a) # receive a set of strings
-        # print(perms)
-        return [[int(i) for i in iter(j).next()] for j in perms]
+        return self.remove_dupes(self.get_all_permutations(A))
 
 s = Solution()
 inp = [1, 2, 3]
