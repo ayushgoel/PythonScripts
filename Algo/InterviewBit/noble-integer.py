e436b4f1bb3d6dbfbbdf5a4d4e43e479e74a5732
Ayush Goel <ayushgoel111@gmail.com>
2017-07-08 01:38:32 +0530

    Add solution for noble-integer

---
+++ Algo/InterviewBit/noble-integer.py
@@ -0,0 +1,22 @@
+import collections
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, a):
+        a = [i for i in a if i >= 0]
+        x=collections.Counter(a)
+        big_nums = 0
+        # print x.items()
+        for val, count in sorted(x.items(),key= lambda q: q[0], reverse=True):
+            # print val, count, big_nums
+            if val == big_nums:
+                return 1
+            if big_nums > val:
+                break
+            big_nums += count
+        return -1
+
+s = Solution()
+print(s.solve([ 4, -9, 8, 5, -1, 7, 5, 3 ])) #1
+# print(s.solve([ -4, 7, 5, 3, 5, -4, 2, -1, -9, -8, -3, 0, 9, -7, -4, -10, -4, 2, 6, 1, -2, -3, -1, -8, 0, -8, -7, -3, 5, -1, -8, -8, 8, -1, -3, 3, 6, 1, -8, -1, 3, -9, 9, -6, 7, 8, -6, 5, 0, 3, -4, 1, -10, 6, 3, -8, 0, 6, -9, -5, -5, -6, -3, 6, -5, -4, -1, 3, 7, -6, 5, -8, -5, 4, -3, 4, -6, -7, 0, -3, -2, 6, 8, -2, -6,-7, 1, 4, 9, 2, -10, 6, -2, 9, 2, -4, -4, 4, 9, 5, 0, 4, 8, -3, -9, 7, -8, 7, 2, 2, 6, -9, -10, -4, -9, -5, -1, -6, 9, -10, -1, 1, 7, 7, 1, -9, 5, -1, -3, -3, 6, 7, 3, -4, -5, -4, -7, 9, -6, -2, 1, 2, -1, -7, 9, 0, -2, -2, 5, -10,-1, 6, -7, 8, -5, -4, 1, -9, 5, 9, -2, -6, -2, -9, 0, 3, -10, 4, -6, -6, 4, -3, 6, -7, 1, -3, -5, 9, 6, 2, 1, 7, -2, 5 ]))
+# print(s.solve())
