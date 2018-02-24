5f2da4a2b3f64dd33816ea910ca11ca3972513bb
Ayush Goel <ayushgoel111@gmail.com>
2018-02-24 16:46:27 +0530

    Add solution for prob #2 in HR contest

---
+++ Algo/HackerRank/university-codesprint-4/cubes-and-cylinders.py
@@ -0,0 +1,41 @@
+
+#!/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+import math
+
+
+# Complete the function below.
+
+def maximumPackages(S, K, R, C):
+    # Return the maximum number of packages that can be put in the containers.
+    ans = 0
+    pack = dict(zip(S,K))
+    con = dict(zip(R,C))
+    lens = sorted(S, reverse=True)
+    rs = sorted(R, reverse=True)
+    for r in rs:
+        possibleL = math.floor(r*1.414)
+        pCaps = con[r]
+        for i in xrange(len(lens)):
+            if pCaps <= 0:
+                break
+            if lens[i] <= possibleL:
+                copies = pack[lens[i]]
+                if pCaps >= copies:
+                    ans += copies
+                    pCaps = pCaps - copies
+                    pack[lens[i]] = 0
+                else:
+                    ans += pCaps
+                    pack[lens[i]] = copies - pCaps
+                    pCaps = 0
+        # delete lens with copies 0
+    return ans
+
+
+if __name__ == '__main__':
+    print(maximumPackages([1,2],[1,3],[1,2],[1,1]))
