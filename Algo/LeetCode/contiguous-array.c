e1b76db01bf7ac4da57f230804d7f5fdf0a53317
Ayush Goel <ayushgoel111@gmail.com>
2018-02-24 22:24:58 +0530

    Update solution for contiguous-array which gets memory exceeded

--- Algo/LeetCode/contiguous-array.c
+++ Algo/LeetCode/contiguous-array.c
@@ -1,48 +1,75 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return a>b?a:b;
+}
+
+int memo[50005][50005];
+
+int findLength(int *nums, int start, int end, int num_zeroes, int num_ones) {
+    // printf("%d %d %d %d %d %d\n", start, end, num_zeroes, num_ones, nums[start], nums[end]);
+    if (memo[start][end] != 0)
+    {
+        return memo[start][end];
+    }
+    if (start == end)
+    {
+        return 0;
+    }
+    if (num_zeroes == num_ones)
+    {
+        memo[start][end] = end-start+1;
+    } else if (num_zeroes > num_ones)
+    {
+        if (nums[start] == 0)
+        {
+            memo[start][end] = findLength(nums, start + 1, end, num_zeroes - 1, num_ones);
+        } else if (nums[end] == 0)
+        {
+            memo[start][end] = findLength(nums, start, end - 1, num_zeroes - 1, num_ones);
+        } else {
+            memo[start][end] = max(findLength(nums, start, end - 1, num_zeroes, num_ones - 1),
+                                   findLength(nums, start + 1, end, num_zeroes, num_ones - 1));
+        }
+    } else
+    {
+        if (nums[start] == 1)
+        {
+            memo[start][end] = findLength(nums, start + 1, end, num_zeroes, num_ones - 1);
+        } else if (nums[end] == 1)
+        {
+            memo[start][end] = findLength(nums, start, end - 1, num_zeroes, num_ones - 1);
+        } else {
+            memo[start][end] = max(findLength(nums, start, end - 1, num_zeroes - 1, num_ones),
+                                   findLength(nums, start + 1, end, num_zeroes - 1, num_ones));
+        }
+    }
+    return memo[start][end];
+}
+
 int findMaxLength(int* nums, int numsSize) {
     if(numsSize <= 1) {
         return 0;
     }
-    int *t0=malloc(numsSize * sizeof(int));
-    int *t1=malloc(numsSize * sizeof(int));
-    int *maxl=malloc(numsSize * sizeof(int));
-
-    if(nums[0] == 0) {
-        t0[0] = 1;
-        t1[0] = 0;
-        maxl[0] = 0;
-    } else {
-        t0[0] = 0;
-        t1[0] = 1;
-        maxl[0] = 0;
-    }
-
-    for(int i=1; i<numsSize; ++i) {
-        if(nums[i] == 0) {
-            t0[i] = t0[i-1] + 1;
-            t1[i] = t1[i-1];
-        } else {
-            t0[i] = t0[i-1];
-            t1[i] = t1[i-1] + 1;
-        }
-        // calculate maxl
-        maxl[i] = -1;
-        if(t0[i] == t1[i]) {
-            maxl[i] = i+1;
-        } else {
-            for(int j = 0; j<i; ++j) {
-                if((t0[i]-t0[j]) == (t1[i]-t1[j])) {
-                    maxl[i] = i-j;
-                    break;
-                }
-            }
+    int num_zeroes = 0, num_ones = 0;
+    for (int i = 0; i < numsSize; ++i)
+    {
+        if (nums[i] == 0)
+        {
+            num_zeroes++;
+        } else
+        {
+            num_ones++;
         }
     }
+    return findLength(nums, 0, numsSize - 1, num_zeroes, num_ones);
+}
 
-    int ans=0;
-    for(int i=1; i<numsSize; ++i) {
-        if(maxl[i] > ans) {
-            ans = maxl[i];
-        }
-    }
-    return ans;
+int main(int argc, char const *argv[])
+{
+    int x[] = {0,1,0,1,1,1,1};
+    printf("%d\n", findMaxLength(x,7));
+    // int x[] = {0,1,0,1,1,1,0,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,1,0,1,0,0,1,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,0,1,1,0,0,0,1,1};
+    // printf("%d\n", findMaxLength(x,100));
+    return 0;
 }
