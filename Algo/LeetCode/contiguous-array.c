30b2e810ab56b779961311334bb838414ad0518a
Ayush Goel <ayushgoel111@gmail.com>
2018-02-24 22:40:37 +0530

    Accepted solution

--- Algo/LeetCode/contiguous-array.c
+++ Algo/LeetCode/contiguous-array.c
@@ -1,68 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int max(int a, int b) {
-    return a>b?a:b;
-}
-
-int memo[50005][50005];
-
-int findLength(int *nums, int start, int end, int num_zeroes, int num_ones) {
-    // printf("%d %d %d %d %d %d\n", start, end, num_zeroes, num_ones, nums[start], nums[end]);
-    if (memo[start][end] != 0)
-    {
-        return memo[start][end];
-    }
-    if (start == end)
-    {
+int findMaxLength(int* nums, int n) {
+    if(n <= 1) {
         return 0;
     }
-    if (num_zeroes == num_ones)
+    int t = 0;
+    int *x = malloc((2 * n + 1) * sizeof(int)); // assume 0
+    for (int i = 0; i < 2*n+1; ++i)
     {
-        memo[start][end] = end-start+1;
-    } else if (num_zeroes > num_ones)
-    {
-        if (nums[start] == 0)
-        {
-            memo[start][end] = findLength(nums, start + 1, end, num_zeroes - 1, num_ones);
-        } else if (nums[end] == 0)
-        {
-            memo[start][end] = findLength(nums, start, end - 1, num_zeroes - 1, num_ones);
-        } else {
-            memo[start][end] = max(findLength(nums, start, end - 1, num_zeroes, num_ones - 1),
-                                   findLength(nums, start + 1, end, num_zeroes, num_ones - 1));
-        }
-    } else
-    {
-        if (nums[start] == 1)
-        {
-            memo[start][end] = findLength(nums, start + 1, end, num_zeroes, num_ones - 1);
-        } else if (nums[end] == 1)
-        {
-            memo[start][end] = findLength(nums, start, end - 1, num_zeroes, num_ones - 1);
-        } else {
-            memo[start][end] = max(findLength(nums, start, end - 1, num_zeroes - 1, num_ones),
-                                   findLength(nums, start + 1, end, num_zeroes - 1, num_ones));
-        }
+        x[i] = -1;
     }
-    return memo[start][end];
-}
-
-int findMaxLength(int* nums, int numsSize) {
-    if(numsSize <= 1) {
-        return 0;
-    }
-    int num_zeroes = 0, num_ones = 0;
-    for (int i = 0; i < numsSize; ++i)
+    int ans = 0;
+    x[n] = 0;
+    for (int i = 0; i < n; ++i)
     {
         if (nums[i] == 0)
         {
-            num_zeroes++;
+            t++;
         } else
         {
-            num_ones++;
+            t--;
+        }
+        if (x[t+n] == -1)
+        {
+            x[t+n] = i+1;
+        } else {
+            int poss = i - x[t+n] + 1;
+            if (poss > ans)
+            {
+                ans = poss;
+            }
         }
     }
-    return findLength(nums, 0, numsSize - 1, num_zeroes, num_ones);
+    return ans;
 }
 
 int main(int argc, char const *argv[])
