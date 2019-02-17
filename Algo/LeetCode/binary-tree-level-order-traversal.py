5c45dc9928c1b3aadaa72e3d6cee05b7f9dc89ff
Ayush Goel <ayushgoel111@gmail.com>
2019-02-17 23:42:50 +0530

    Add better solution for binary-tree-level-order-traversal
    Even though a for loop is reduced, the solution is slower
    since it removes element from 0th index in array which
    is an O(n) operation.

--- Algo/LeetCode/binary-tree-level-order-traversal.py
+++ Algo/LeetCode/binary-tree-level-order-traversal.py
@@ -12,23 +12,14 @@ class Solution:
         if root is None:
             return []
         q=[(root,0)]
-        cur = 0
-        while True:
-            if cur >= len(q):
-                break
-            # print(cur, len(q))
-            n, lvl = q[cur]
+        ans = []
+        while len(q) > 0:
+            n, lvl = q.pop(0)
             if n.left is not None:
                 q.append((n.left, lvl+1))
             if n.right is not None:
                 q.append((n.right, lvl+1))
-            cur += 1
-        # print([[i[0].val, i[1]] for i in q])
-        ans = []
-        for i in q:
-            if i[1] >= len(ans):
+            if lvl >= len(ans):
                 ans += [[]]
-            ans[i[1]] += [i[0].val]
+            ans[lvl] += [n.val]
         return ans
-            
-        
