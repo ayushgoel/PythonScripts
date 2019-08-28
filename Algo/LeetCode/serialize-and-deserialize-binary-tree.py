d58754b9f24d83aafd7685bd04a31ab1c2b17576
Ayush Goel <ayushgoel111@gmail.com>
2019-08-28 22:03:08 +0530

    Add accepted solution for serialize-and-deserialize-binary-tree

--- Algo/LeetCode/serialize-and-deserialize-binary-tree.py
+++ Algo/LeetCode/serialize-and-deserialize-binary-tree.py
@@ -1,24 +1,12 @@
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Codec:
-    
-    def ser(self, root, ind, arr):
-        # print(ind, arr)
-        if root is None:
-            return
-        arr[ind] = str(root.val)
-        self.ser(root.left, 2*ind, arr)
-        self.ser(root.right, (2*ind)+1, arr)
-    
-    def height(self, root):
-        if root is None:
-            return 0
-        return (1+max(self.height(root.left), self.height(root.right)))
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -26,19 +14,25 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        h = self.height(root)
-        # print(h)
-        arr = ["null"] * (2**h)
-        self.ser(root, 1, arr)
-        return ",".join(arr)
-        
-    def dser(self, ind, arr):
-        if ind >= len(arr) or arr[ind] == "null":
-            return None
-        nd = TreeNode(int(arr[ind]))
-        nd.left = self.dser(2*ind, arr)
-        nd.right = self.dser((2*ind)+1, arr)
-        return nd
+        if root is None:
+            return ""
+        enc = [str(root.val)]
+        q = [root]
+        while len(q) != 0:
+            n = q.pop(0)
+            if n.left is not None:
+                enc.append(str(n.left.val))
+                q.append(n.left)
+            else:
+                enc.append("null")
+
+            if n.right is not None:
+                enc.append(str(n.right.val))
+                q.append(n.right)
+            else:
+                enc.append("null")
+        return ",".join(enc)
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -46,11 +40,48 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        ns = data.split(",")
-        return self.dser(1, ns)
-            
+        if len(data) == 0:
+            return None
+        dec = data.split(",")
+        root = TreeNode(int(dec[0]))
+        q=[root]
+        ind  = 1
+        while len(q)!=0:
+            n = q.pop(0)
+            if dec[ind] != "null":
+                t = TreeNode(int(dec[ind]))
+                n.left = t
+                q.append(t)
+            ind += 1
+            if dec[ind] != "null":
+                t = TreeNode(int(dec[ind]))
+                n.right = t
+                q.append(t)
+            ind += 1
+        return root
+
+
         
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
+
+n = TreeNode(2)
+n.left = TreeNode(1)
+n.right = TreeNode(3)
+n.right.right = TreeNode(4)
+
+c = Codec()
+a = c.serialize(n)
+assert(a == "2,1,3,null,null,null,4,null,null")
+
+b = c.deserialize(a)
+assert(b.val == 2)
+assert(b.left.val == 1)
+assert(b.right.val == 3)
+assert(b.right.right.val == 4)
+assert(b.right.right.left == None)
+
+print(c.serialize(None))
+print(c.deserialize(c.serialize(None)))
